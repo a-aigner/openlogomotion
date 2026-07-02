8c09616fcb44fb830dc64b9cb9ed1d23ddb4a5d9
a-aigner <andre.onephone@gmail.com>
2026-07-02 14:17:31 +0200

    feat: Root calculateMetadata uses totalFrames to include outro hold

--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -1,18 +1,18 @@
 import { Composition } from "remotion";
 import { ShowcaseComposition } from "./ShowcaseComposition";
-import { DEFAULT_SHOWCASE_CONFIG, type ShowcaseConfig } from "@/lib/showcase-config";
+import { DEFAULT_SHOWCASE_CONFIG, totalFrames, type ShowcaseConfig } from "@/lib/showcase-config";
 
 export const RemotionRoot: React.FC = () => (
   <Composition
     id="LogoShowcase"
     component={ShowcaseComposition}
-    durationInFrames={DEFAULT_SHOWCASE_CONFIG.format.durationInFrames}
+    durationInFrames={totalFrames(DEFAULT_SHOWCASE_CONFIG)}
     fps={DEFAULT_SHOWCASE_CONFIG.format.fps}
     width={DEFAULT_SHOWCASE_CONFIG.format.width}
     height={DEFAULT_SHOWCASE_CONFIG.format.height}
     defaultProps={{ config: DEFAULT_SHOWCASE_CONFIG }}
     calculateMetadata={({ props }: { props: { config: ShowcaseConfig } }) => ({
-      durationInFrames: props.config.format.durationInFrames,
+      durationInFrames: totalFrames(props.config),
       fps: props.config.format.fps,
       width: props.config.format.width,
       height: props.config.format.height,
